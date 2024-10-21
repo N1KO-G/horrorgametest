94d892933ecd07093e84f8b9ab855081c304e1c8
N1KO-G <146829154+N1KO-G@users.noreply.github.com>
2024-10-21 23:16:24 +0100

    enemy

--- Assets/scripts/enemyscripttest.cs
+++ Assets/scripts/enemyscripttest.cs
@@ -1,20 +1,37 @@
 using System;
+using Unity.Mathematics;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class enemyscripttest : MonoBehaviour
 {
-
-public Transform player;
-public NavMeshAgent navMeshAgent;
+    public Transform Player;
+    public float moveSpeed = 3f;
+    public float Maxdistance = 10f;
+    public float Mindistance = 10f;
 
 public void Awake()
 {
-    navMeshAgent = GetComponent<NavMeshAgent>();
+ 
 }
 
 public void Update()
 {
-    navMeshAgent.SetDestination(player.transform.position);
+
+  transform.LookAt(Player);
+  transform.localEulerAngles = new Vector3(0f,transform.localEulerAngles.y, transform.localEulerAngles.z);
+
+
+  if(Vector3.Distance(transform.position, Player.position) >= Mindistance)
+  {
+    transform.position += transform.forward * moveSpeed * Time.deltaTime;
+
+
+    if(Vector3.Distance(transform.position, Player.position) >= Maxdistance)
+    {
+       
+    }
+  }
 }
 }
